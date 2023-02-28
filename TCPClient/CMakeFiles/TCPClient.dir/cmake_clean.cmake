392f9390e5d7b219f2d643b427c50529e863e9b0
Mohamed Ghanem <106385425+mghanem8@users.noreply.github.com>
2023-02-28 13:11:58 -0500

    Add files via upload

---
+++ TCPClient/CMakeFiles/TCPClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "."
+  "/Users/ghanem/Desktop/ECE4122/TCPClient.pdb"
+  "CMakeFiles/TCPClient.dir/main.cpp.o"
+  "CMakeFiles/TCPClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCPClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TCPServer/build/CMakeFiles/TCPServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/ghanem/Desktop/ECE4122/TCPServer/TCPServer"
+  "/Users/ghanem/Desktop/ECE4122/TCPServer/TCPServer.pdb"
+  "CMakeFiles/TCPServer.dir/main.cpp.o"
+  "CMakeFiles/TCPServer.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCPServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
